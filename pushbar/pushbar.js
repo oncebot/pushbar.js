b103ed66207ce3cbfe876159953407f4cf211026
oncebot <hi@oncebot.com>
2019-12-04 12:54:56 +0530

    beautify code and added changes to core files for v2

--- pushbar/pushbar.js
+++ pushbar/pushbar.js
@@ -1,117 +1,80 @@
 class Pushbar {
-  constructor(config = { overlay: true, blur: false }) {
-    this.activeBar = null;
-    this.overlay = false;
-
-    if (config.overlay) {
-      this.overlay = document.createElement('div');
-      this.overlay.classList.add('pushbar_overlay');
-      document.querySelector('body').appendChild(this.overlay);
+    constructor(config = { overlay: true, blur: false }) {
+        this.activeId;
+        this.activeElement;
+        this.overlayElement;
+        if (config.overlay) {
+            this.overlayElement = document.createElement('div');
+            this.overlayElement.classList.add('pushbar_overlay');
+            document.querySelector('body').appendChild(this.overlayElement);
+        }
+        if (config.blur) {
+            const mainContent = document.querySelector('.pushbar_main_content');
+            if (mainContent) {
+                mainContent.classList.add('pushbar_blur');
+            }
+        }
+        this.bindEvents();
     }
 
-    if (config.blur) {
-      const mainContent = document.querySelector('.pushbar_main_content');
-      if (mainContent) {
-        mainContent.classList.add('pushbar_blur');
-      }
+    emitOpening() {
+        const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { pushbar: this.activeElement } });
+        this.activeElement.dispatchEvent(event);
     }
 
-    this.bindEvents();
-  }
-
-  get opened() {
-    const { activeBar } = this;
-    return Boolean(activeBar instanceof HTMLElement && activeBar.classList.contains('opened'));
-  }
-  
-  get activeBarId() {
-    const { activeBar } = this;
-    return activeBar instanceof HTMLElement && activeBar.getAttribute('data-pushbar-id');
-  }
-
-  static dispatchOpen(pushbar) {
-    const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { pushbar } });
-    pushbar.dispatchEvent(event);
-  }
-
-  static dispatchClose(pushbar) {
-    const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { pushbar } });
-    pushbar.dispatchEvent(event);
-  }
-
-  static findElementById(pushbarId) {
-    return document.querySelector(`[data-pushbar-id="${pushbarId}"]`);
-  }
-
-  handleOpenEvent(e) {
-    e.preventDefault();
-    const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
-    if (pushbarId) {
-      this.open(pushbarId);
+    emitClosing() {
+        const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { pushbar: this.activeElement } });
+        this.activeElement.dispatchEvent(event);
     }
-  }
 
-  handleCloseEvent(e) {
-    e.preventDefault();
-    this.close();
-  }
-
-  handleKeyEvent(e) {
-    if (this.opened && e.keyCode === 27) {
-      this.close();
+    handleOpenEvent(e) {
+        e.preventDefault();
+        const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
+        this.open(pushbarId);
     }
-  }
 
-  bindEvents() {
-    const triggers = document.querySelectorAll('[data-pushbar-target]');
-    const closers = document.querySelectorAll('[data-pushbar-close]');
-
-    triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
-    closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
-
-    if (this.overlay) {
-      this.overlay.addEventListener('click', e => this.handleCloseEvent(e), false);
+    handleCloseEvent(e) {
+        e.preventDefault();
+        this.close();
     }
-    document.addEventListener('keyup', e => this.handleKeyEvent(e));
-  }
 
-  open(pushbarId) {
-    // Current bar is already opened
-    if (String(pushbarId) === this.activeBarId && this.opened) {
-      return;
+    handleKeyEvent(e) {
+        if (e.keyCode === 27) this.close();
     }
-    
-    // Get new pushbar target
-    const pushbar = Pushbar.findElementById(pushbarId);
 
-    if (!pushbar) return;
-    
-    // Close active bar (if exists)
-    if (this.opened) {
-      this.close();
+    bindEvents() {
+        const triggers = document.querySelectorAll('[data-pushbar-target]');
+        const closers = document.querySelectorAll('[data-pushbar-close]');
+        triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
+        closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
+        if (this.overlayElement) {
+            this.overlayElement.addEventListener('click', e => this.handleCloseEvent(e), false);
+        }
+        document.addEventListener('keyup', e => this.handleKeyEvent(e));
     }
-    
-    Pushbar.dispatchOpen(pushbar);
-    pushbar.classList.add('opened');
 
-    const Root = document.querySelector('html');
-    Root.classList.add('pushbar_locked');
-    Root.setAttribute('pushbar', pushbarId);
-    this.activeBar = pushbar;
-  }
+    open(pushbarId) {
+        if (this.activeId === String(pushbarId) || !pushbarId) return;
+        if (this.activeId && this.activeId !== String(pushbarId)) this.close();
+        this.activeId = pushbarId
+        this.activeElement = document.querySelector(`[data-pushbar-id="${this.activeId}"]`)
+        if (!this.activeElement) return;
+        this.emitOpening();
+        this.activeElement.classList.add('opened');
+        const pageRootElement = document.querySelector('html')
+        pageRootElement.classList.add('pushbar_locked');
+        pageRootElement.setAttribute('pushbar', pushbarId)
+        this.activeId = pushbarId;
+    }
 
-  close() {
-    const { activeBar } = this;
-    if (!activeBar) return;
-    
-    Pushbar.dispatchClose(activeBar);
-    activeBar.classList.remove('opened');
-
-    const Root = document.querySelector('html');
-    Root.classList.remove('pushbar_locked');
-    Root.removeAttribute('pushbar');
-    
-    this.activeBar = null;
-  }
+    close() {
+        if (!this.activeId) return;
+        this.emitClosing();
+        this.activeElement.classList.remove('opened');
+        const pageRootElement = document.querySelector('html')
+        pageRootElement.classList.remove('pushbar_locked');
+        pageRootElement.removeAttribute('pushbar')
+        this.activeId = null;
+        this.activeElement = null;
+    }
 }
-
